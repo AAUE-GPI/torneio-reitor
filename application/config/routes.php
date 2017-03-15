6e52a572501917b65179122e0cebfd430d892245
NunoLopes96 <nun0lopes@hotmail.com>
2017-03-15 21:50:54 +0000

    Bug fix dos routes

--- application/config/routes.php
+++ application/config/routes.php
@@ -49,7 +49,9 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | Examples:	my-controller/index	-> my_controller/index
 |		my-controller/my-method	-> my_controller/my_method
 */
-$route['default_controller'] = 'FAQ';
+$route['default_controller'] = 'calendario/index';
+$route['cona'] = 'calendario/index';
+
 $route['calendario'] = 'calendario';
 $route['FAQ'] = 'FAQ';
 $route['404_override'] = '';
