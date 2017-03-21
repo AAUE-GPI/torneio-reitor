71ec8fa173f35a42952af43d4358096cfc15a63e
root <root@monolith>
2017-03-21 15:38:26 +0000

    fix na identação - 2

--- application/views/noticias.php
+++ application/views/noticias.php
@@ -1,33 +1,32 @@
 
-  <div class="container">
-    <h1 class="titulo">Notícias</h1>
-      <div class="panel-group" id="accordion" >
-        <div class="panel panel-faq">
+<div class="container">
+  <h1 class="titulo">Notícias</h1>
 
-
-        <?php
-        $i=0;
-        foreach($noticias as $noticias):?>
-          <a data-toggle="collapse" data-parent="#accordion" href="#collapse<?=$i?>" style="color: inherit;"">
-            <div class="panel-heading panel-heading-<?php echo $i%2; ?>">
-              <div class="row">
-                <div class="col-xs-4">
-                  <h4 class="panel-title" >
+  <div class="panel-group" id="accordion" >
+    <div class="panel panel-faq">
+      <?php
+      $i=0;
+      foreach($noticias as $noticias):?>
+        <a data-toggle="collapse" data-parent="#accordion" href="#collapse<?=$i?>" style="color: inherit;"">
+          <div class="panel-heading panel-heading-<?php echo $i%2; ?>">
+            <div class="row">
+              <div class="col-xs-4">
+                <h4 class="panel-title" >
                   <?php print_r( $noticias['data'] ); ?>
-                  </h4>
-                </div>
-                <div class="col-xs-7">
-                  <h4 class="panel-title" >
-                    <?php print_r( $noticias['titulo'] ); ?>
-                  </h4>
-                </div>
+                </h4>
+              </div>
+              <div class="col-xs-7">
+                <h4 class="panel-title" >
+                  <?php print_r( $noticias['titulo'] ); ?>
+                </h4>
+              </div>
 
-                <div class="col-xs-1">
-                  <span class="glyphicon glyphicon-menu-down seta-baixo-<?php echo $i%2; ?>"></span>
-                </div>
+              <div class="col-xs-1">
+                <span class="glyphicon glyphicon-menu-down seta-baixo-<?php echo $i%2; ?>"></span>
               </div>
             </div>
-          </a>
+          </div>
+        </a>
 
         <div id="collapse<?=$i?>" class="panel-collapse collapse">
           <div class="panel-body panel-body-<?php echo $i%2; ?>">
@@ -35,8 +34,8 @@
 
           </div>
         </div>
-        <?php $i++; endforeach; ?>
 
-      </div>
+      <?php $i++; endforeach; ?>
     </div>
   </div>
+</div>
